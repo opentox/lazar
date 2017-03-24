2cebe2c0dc60bda0c94d0d9c38e7931efadd7155
gebele <gebele@in-silico.ch>
2017-03-24 12:51:12 +0000

    adjusted for higher similarity min value

--- test/model-classification.rb
+++ test/model-classification.rb
@@ -10,35 +10,35 @@ class LazarClassificationTest < MiniTest::Test
       },
       :similarity => {
         :method => "Algorithm::Similarity.tanimoto",
-        :min => 0.1
+        :min => 0.5
       },
+      :feature_selection => nil,
       :prediction => {
         :method => "Algorithm::Classification.weighted_majority_vote",
       },
-      :feature_selection => nil,
     }
     training_dataset = Dataset.from_csv_file File.join(DATA_DIR,"hamster_carcinogenicity.csv")
     model = Model::Lazar.create  training_dataset: training_dataset
     assert_kind_of Model::LazarClassification, model
     assert_equal algorithms, model.algorithms
-    substance = training_dataset.substances[10]
+    substance = training_dataset.substances[49]
     prediction = model.predict substance
     assert_equal "false", prediction[:value]
     [ {
-      :compound => OpenTox::Compound.from_inchi("InChI=1S/C6H6/c1-2-4-6-5-3-1/h1-6H"),
+      :compound => OpenTox::Compound.from_inchi("InChI=1S/C6H14N2O4/c1-5(10)2-8(7-12)3-6(11)4-9/h5-6,9-11H,2-4H2,1H3"),
       :prediction => "false",
     },{
-      :compound => OpenTox::Compound.from_smiles("c1ccccc1NN"),
+      :compound => OpenTox::Compound.from_smiles("OCC(CN(CC(O)C)N=O)O"),
       :prediction => "false",
     } ].each do |example|
       prediction = model.predict example[:compound]
       assert_equal example[:prediction], prediction[:value]
     end
 
-    compound = Compound.from_smiles "CCO"
+    compound = Compound.from_smiles "O=NN1CCC1"
     prediction = model.predict compound
     assert_equal "true", prediction[:value]
-    assert_equal ["false"], prediction[:measurements]
+    #assert_equal ["false"], prediction[:measurements]
 
     # make a dataset prediction
     compound_dataset = OpenTox::Dataset.from_csv_file File.join(DATA_DIR,"EPAFHM.mini_log10.csv")
@@ -46,12 +46,12 @@ class LazarClassificationTest < MiniTest::Test
     assert_equal compound_dataset.compounds, prediction_dataset.compounds
 
     cid = prediction_dataset.compounds[7].id.to_s
-    assert_equal "Could not find similar substances with experimental data in the training dataset.", prediction_dataset.predictions[cid][:warning]
+    assert_equal "Could not find similar substances with experimental data in the training dataset.", prediction_dataset.predictions[cid][:warnings][0]
     prediction_dataset.predictions.each do |cid,pred|
-      assert_equal "Could not find similar substances with experimental data in the training dataset.", pred[:warning] if pred[:value].nil?
+      assert_equal "Could not find similar substances with experimental data in the training dataset.", pred[:warnings][0] if pred[:value].nil?
     end
     cid = Compound.from_smiles("CCOC(=O)N").id.to_s
-    assert_match "excluded", prediction_dataset.predictions[cid][:warning]
+    assert_match "excluded", prediction_dataset.predictions[cid][:info]
     # cleanup
     [training_dataset,model,compound_dataset,prediction_dataset].each{|o| o.delete}
   end
@@ -85,7 +85,7 @@ class LazarClassificationTest < MiniTest::Test
     model = Model::Lazar.create training_dataset: training_dataset
     t = Time.now
     2.times do
-      compound = Compound.from_smiles("Clc1ccccc1NN")
+      compound = Compound.from_smiles("OCC(CN(CC(O)C)N=O)O")
       prediction = model.predict compound
       assert_equal "1", prediction[:value]
     end
