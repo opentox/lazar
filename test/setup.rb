3bb7797f22925eb04c057aab64e62b6c5af67edb
gebele <gebele@in-silico.ch>
2018-01-17 14:49:01 +0100

    unmuted enm import; mute cleanup

--- test/setup.rb
+++ test/setup.rb
@@ -3,10 +3,10 @@ require 'minitest/autorun'
 require_relative '../lib/lazar.rb'
 #require 'lazar'
 include OpenTox
-$mongo.database.drop
-$gridfs = $mongo.database.fs # recreate GridFS indexes
+#$mongo.database.drop
+#$gridfs = $mongo.database.fs # recreate GridFS indexes
 PhysChem.descriptors
 TEST_DIR ||= File.expand_path(File.dirname(__FILE__))
 DATA_DIR ||= File.join(TEST_DIR,"data")
-#training_dataset = Dataset.where(:name => "Protein Corona Fingerprinting Predicts the Cellular Interaction of Gold and Silver Nanoparticles").first
-#Import::Enanomapper.import unless training_dataset
+training_dataset = Dataset.where(:name => "Protein Corona Fingerprinting Predicts the Cellular Interaction of Gold and Silver Nanoparticles").first
+Import::Enanomapper.import unless training_dataset
