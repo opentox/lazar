43b804cd820c9feb2b7844524a424ed6fc0df3e5
gebele <gebele@in-silico.ch>
2019-02-20 11:36:40 +0000

    removed probability hack

--- lib/classification.rb
+++ lib/classification.rb
@@ -18,11 +18,6 @@ module OpenTox
         class_weights.each do |a,w|
           probabilities[a] = w.sum/weights.sum
         end
-        # DG: hack to ensure always two probability values
-        if probabilities.keys.uniq.size == 1
-          missing_key = probabilities.keys.uniq[0].match(/^non/) ? probabilities.keys.uniq[0].sub(/non-/,"") : "non-"+probabilities.keys.uniq[0]
-          probabilities[missing_key] = 0.0
-        end
         probabilities = probabilities.collect{|a,p| [a,weights.max*p]}.to_h
         p_max = probabilities.collect{|a,p| p}.max
         prediction = probabilities.key(p_max)
